2e505502bf36d6d0d117dc5d3ea965fe3678cab2
Raja Mahanty <rajamahanty2@gmail.com>
2026-03-20 09:35:31 +0530

    feat: implement user authentication with registration and login functionality

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import app from "./src/app.js";
 import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+await connectDB();
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
+// REGISTER
+export const register = async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+
+		const exists = await User.findOne({ email });
+		if (exists) {
+			return res.status(400).json({ msg: "User already exists" });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const user = await User.create({
+			name,
+			email,
+			password: hashedPassword,
+		});
+
+		res.status(201).json({
+			token: generateToken(user._id),
+		});
+	} catch (err) {
+		res.status(500).json({ msg: err.message });
+	}
+};
+
+// LOGIN
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ msg: "Invalid credentials" });
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) {
+			return res.status(400).json({ msg: "Invalid credentials" });
+		}
+
+		res.json({
+			token: generateToken(user._id),
+		});
+	} catch (err) {
+		res.status(500).json({ msg: err.message });
+	}
+};
